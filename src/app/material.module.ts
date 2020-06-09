7eb4d328a1e879b83fc2855ff0acca25116b5050
juancampo95 <juandesigner95@gmail.com>
2020-06-08 23:52:40 -0500

    non

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
-import {
-  MatButtonModule,
-  MatToolbarModule,
-  MatSidenavModule,
-  MatListModule,
-  MatDividerModule,
-  MatIconModule,
-  MatTableModule,
-  MatExpansionModule,
-  MatFormFieldModule,
-  MatInputModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const materialComponents = [
   MatButtonModule,
